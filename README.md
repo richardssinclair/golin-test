c88d02b0ce17c21d9d1b515d173833a9171f33b0
Richard Sinclair <richardssinclair@gmail.com>
2018-05-25 14:08:16 +0100

    added functionality to the form

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-I followed this tutorial to setup web-pack with Bootstrap 4 to get me started.
+# Setup instructions:
+
+```
+npm install && npm run build
+```
+```
+open dist/index.html
+```
+
+# Approach:
+
+I used Bootstrap because it is a framework I am familiar with and I know I could get the template up quickly. I decided to use jQuery just to practice my skills and recap how to use it.
+
+I haven't used Webpack before but I followed this tutorial to setup Web-pack with Bootstrap 4 to get me started.
 https://stevenwestmoreland.com/2018/01/how-to-include-bootstrap-in-your-project-with-webpack.html
+
+I haven't had much experience of making validation so i used this video to learn how to do it.
+https://www.youtube.com/watch?v=uQfKJjqToTo
--- src/app.js
+++ src/app.js
@@ -1,13 +1,84 @@
 import 'bootstrap';
 import './scss/app.scss';
 import $ from 'jquery';
-// console.log("here")
-$('#text').on('input', function(){
-  name = $(this).val();
+
+var formValid = {
+  name: false
+};
+
+function checkValidation() {
+  if (formValid.name) {
+    $('#submit-btn').removeAttr('disabled');
+  } else {
+    $('#submit-btn').attr('disabled', true);
+  }
+}
+
+function msg(body) {
+  $('#name-error').text(body).show();
+};
+
+function hide() {
+  $('#name-error').hide();
+};
+
+$('#nameInput').on('input', function(){
+  var name = $(this).val();
 
   if (name.length < 1){
-    alert("field is required")
+    msg('This field is required');
+    formValid.name = false;
+    checkValidation();
   } else {
-
+    hide();
+    formValid.name = true;
+    checkValidation();
+    if (name.length <3 || name.length > 20) {
+      msg('must be between 3 and 20 charecters');
+      formValid.name = false;
+      checkValidation();
+    } else {
+      hide();
+      formValid.name = true;
+      checkValidation();
+    }
   }
+});
+
+function update() {
+  var amountNeeded = total - currentTotal;
+  $("#amountLeft").text(amountNeeded);
+  $("#days").text(daysLeft);
+  $("#donors").text(donors);
+  var percentage = currentTotal / total * 100;
+  $('#progress').css("width", percentage + "%" );
+
+}
+var daysLeft = 3;
+
+var total = 600;
+
+var currentTotal = 433;
+
+var donors = 42;
+
+update();
+
+$( "#form" ).submit(function( event ) {
+
+  var amount = parseInt($('#amount').val());
+
+  currentTotal = currentTotal + amount;
+
+  donors = donors +1;
+
+  update();
+
+  if (currentTotal == 600){
+    console.log("great you reached your goal");
+  } else {
+    console.log("you need more money");
+  }
+
+  event.preventDefault();
 });
